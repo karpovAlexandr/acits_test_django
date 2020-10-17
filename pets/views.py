ac6a0ad080399a64da1405b865908e38f0536fdc
alexandr <karpov.a1exandr@yandex.ru>
2020-10-17 14:28:13 +0300

    add generic views for pets apps

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import (
+    CreateView, DeleteView,
+    DetailView, ListView,
+    UpdateView
+)
 
-# Create your views here.
+from pets.models import Pet
+
+
+class PetListView(ListView):
+    """Вью для всех животных"""
+    model = Pet
+    context_object_name = 'pets'
+
+
+class PetDetailView(DetailView):
+    """Вью для животного"""
+    model = Pet
+    context_object_name = 'pet'
+
+
+class PetCreateView(LoginRequiredMixin, CreateView):
+    """Вью создания животного"""
+    model = Pet
+    fields = '__all__'
+
+
+class PetUpdateView(LoginRequiredMixin, UpdateView):
+    """Вью для редактирования живтного"""
+    model = Pet
+    fields = '__all__'
+
+
+class PetDeleteView(LoginRequiredMixin, DeleteView):
+    """Вью удяления живтного"""
+    model = Pet
+    context_object_name = 'pet'
+
+    def get_success_url(self):
+        return reverse('pets:home')
+
