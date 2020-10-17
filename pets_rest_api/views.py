62d11ae57e49c73fd4b43359bd79e7489de3f998
alexandr <karpov.a1exandr@yandex.ru>
2020-10-17 16:14:24 +0300

    add views for DRF

--- pets_rest_api/views.py
+++ pets_rest_api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from pets.models import Pet
+from pets_rest_api.serializers import (
+    PetCreateSerializer,
+    PetDeleteSerializer,
+    PetDetailSerializer,
+    PetListSerializer,
+    PetUpdateSerializer,
+)
+
+
+class ListPets(generics.ListAPIView):
+    """Вывод списка животных"""
+    queryset = Pet.objects.all()
+    serializer_class = PetListSerializer
+
+
+class DetailPet(generics.RetrieveAPIView):
+    """Вью для просмотра животного"""
+    queryset = Pet.objects.all()
+    serializer_class = PetDetailSerializer
+
+
+class CreatePet(generics.CreateAPIView):
+    """Вью для добавления животного"""
+    queryset = Pet.objects.all()
+    serializer_class = PetCreateSerializer
+
+
+class UpdatePet(generics.UpdateAPIView):
+    """Вью для редактирования животного"""
+    queryset = Pet.objects.all()
+    serializer_class = PetUpdateSerializer
+
+
+class DeletePet(generics.DestroyAPIView):
+    """Вью для удаления животного"""
+    queryset = Pet.objects.all()
+    serializer_class = PetDeleteSerializer
