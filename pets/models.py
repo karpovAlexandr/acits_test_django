fbcae54840d0083e1b89ece87f0dc9b79fefe368
alexandr <karpov.a1exandr@yandex.ru>
2020-10-17 14:27:07 +0300

    add pet model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Pet(models.Model):
+    """Модель животного"""
+    name = models.CharField('Кличка', max_length=50)
+    age = models.SmallIntegerField('Возраст')
+    arrived = models.DateField('Дата прибытия в приют', auto_now_add=True)
+    weight = models.FloatField('Вес')
+    height = models.SmallIntegerField('Рост')
+    special_sigh = models.CharField('Особые приметы', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('pets:pet_detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = 'Животное'
+        verbose_name_plural = 'Животные'
+
