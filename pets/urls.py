c573b4a7a7232ec5d70b585bcca8f8c934c1bd47
alexandr <karpov.a1exandr@yandex.ru>
2020-10-17 14:28:29 +0300

    urls done

---
+++ pets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from pets import views
+
+app_name = 'pets'
+
+urlpatterns = [
+    path('', views.PetListView.as_view(), name='home'),
+    path('pet/<int:pk>/', views.PetDetailView.as_view(), name='pet_detail'),
+    path('pet/add/', views.PetCreateView.as_view(), name='pet_create'),
+    path('pet/<int:pk>/update', views.PetUpdateView.as_view(), name='pet_update'),
+    path('pet/<int:pk>/delete', views.PetDeleteView.as_view(), name='pet_delete'),
+]
