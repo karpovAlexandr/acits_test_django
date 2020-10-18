d1f811879de49f55d33807f1d75175a0d86f2f42
alexandr <karpov.a1exandr@yandex.ru>
2020-10-18 16:40:20 +0300

    register Shelter and Profiles models

--- pets/admin.py
+++ pets/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 
-from pets.models import Pet
+from pets.models import Pet, Profile, Shelter
+
+admin.site.register(Profile)
 
 
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
     """Модель животного для админки"""
-    list_display = ('id', 'name', 'age', 'arrived', 'weight', 'height', 'special_sigh',)
-    fields = ('name', 'age', 'arrived', 'weight', 'height', 'special_sigh',)
+    list_display = ('id', 'name', 'arrived', 'shelter', 'special_sigh',)
+    fields = ('name', 'age', 'arrived', 'weight', 'height', 'special_sigh', 'shelter',)
     list_display_links = ('id', 'name',)
     search_fields = ('id', 'name', 'special_sigh',)
     readonly_fields = ('arrived',)
 
+
+@admin.register(Shelter)
+class ShelterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    fields = ('title',)
+    list_display_links = ('id', 'title',)
