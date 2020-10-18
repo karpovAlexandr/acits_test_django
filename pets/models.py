8d1031a10dd353514fd42e9157a1ead0594a2467
alexandr <karpov.a1exandr@yandex.ru>
2020-10-18 16:40:00 +0300

    add Shelter and Profile models

--- pets/models.py
+++ pets/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 from django.urls import reverse
 from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+
+
+class Shelter(models.Model):
+    title = models.CharField('Название приюта', max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Приют'
+        verbose_name_plural = 'Приюты'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    shelter = models.ForeignKey(Shelter, related_name='user_shelter', verbose_name='Приют', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
 
 class Pet(models.Model):
@@ -11,6 +35,7 @@ class Pet(models.Model):
     weight = models.FloatField('Вес', validators=[MinValueValidator(0)])
     height = models.SmallIntegerField('Рост', validators=[MinValueValidator(0)])
     special_sigh = models.CharField('Особые приметы', max_length=255, blank=True)
+    shelter = models.ForeignKey(Shelter, related_name='pet_shelter', verbose_name='Приют', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
